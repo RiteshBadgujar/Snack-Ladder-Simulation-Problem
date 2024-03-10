451b7729d3b19478c6b32a9b578d779d7f8444c9
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-10 19:38:29 +0530

    [ADDED]:Ritesh  The Wellcome Massage show

---
+++ src/com/ritesh/snackladdersimulation/SnackLadderSimulaion.java
@@ -0,0 +1,11 @@
+package com.ritesh.snackladdersimulation;
+
+public class SnackLadderSimulaion {
+
+	public static void main(String[] args) {
+		
+	  System.out.println("Wellcome to Snack Ladder Simulation develop by : Ritesh Badgujar");
+
+	}
+
+}

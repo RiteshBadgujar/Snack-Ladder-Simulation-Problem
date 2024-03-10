d506c45cb0da8842714ff0a442be3d542b3028bb
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-10 20:09:31 +0530

    [ADDED]:Ritesh The Show Player Posion With 0

--- src/com/ritesh/snackladdersimulation/SnackLadderSimulaion.java
+++ src/com/ritesh/snackladdersimulation/SnackLadderSimulaion.java
@@ -1,11 +1,20 @@
 package com.ritesh.snackladdersimulation;
 
 public class SnackLadderSimulaion {
+	private  int playerPosition=0;
+	
+	public void ShowStatus() {
+		
+		System.out.println("Player Position = "+playerPosition);
+	}
 
 	public static void main(String[] args) {
 		
 	  System.out.println("Wellcome to Snack Ladder Simulation develop by : Ritesh Badgujar");
-
+	  
+	  SnackLadderSimulaion  player1=new  SnackLadderSimulaion();
+	  
+	  player1.ShowStatus();
 	}
 
 }

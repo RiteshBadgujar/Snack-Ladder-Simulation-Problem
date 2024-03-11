714558083779731073315d999796a157215add9a
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-11 20:18:55 +0530

    [MODIFIED]:Ritesh The Player Start roll die

--- src/com/ritesh/snackladdersimulation/SnackLadderSimulaion.java
+++ src/com/ritesh/snackladdersimulation/SnackLadderSimulaion.java
@@ -1,20 +1,34 @@
 package com.ritesh.snackladdersimulation;
 
+import java.util.Random;
+
 public class SnackLadderSimulaion {
+	
 	private  int playerPosition=0;
+	private int die;
 	
 	public void ShowStatus() {
 		
 		System.out.println("Player Position = "+playerPosition);
 	}
+	
+	public void Playgame() {
+		
+		Random random = new Random();
+		
+		die=random.nextInt(6)+1;
+		System.out.println("Die NO = "+die);
+		
+	}
 
 	public static void main(String[] args) {
 		
-	  System.out.println("Wellcome to Snack Ladder Simulation develop by : Ritesh Badgujar");
+	  System.out.println("Wellcome to Snack Ladder Simulation");
 	  
-	  SnackLadderSimulaion  player1=new  SnackLadderSimulaion();
+	  SnackLadderSimulaion  player1 = new  SnackLadderSimulaion();
 	  
 	  player1.ShowStatus();
+	  player1.Playgame();
 	}
 
 }
